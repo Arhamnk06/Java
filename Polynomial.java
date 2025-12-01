a223990ea87626c5fc5438dbeb0c8bf796a44598
Arhamnk06 <arham.n.khan@torontomu.ca>
2025-12-01 01:30:41 -0500

    Add files via upload

--- Polynomial.java
+++ Polynomial.java
@@ -1,70 +1,131 @@
 import java.util.Arrays;
 
-public class Polynomial {
+public class Polynomial implements Comparable<Polynomial> 
+{
+
     private final int[] coeff;
+    
+    public Polynomial(int[] coefficients) 
+    {
+        
+        int degree = coefficients.length - 1;
+        
+            while (degree > 0 && coefficients[degree] == 0) {
+                degree--;  
+            }
 
-    public Polynomial(int[] coefficients) {
-        int lastNonZero = coefficients.length - 1;
-        while (lastNonZero > 0 && coefficients[lastNonZero] == 0) {
-            lastNonZero--;
-        }
-        coeff = Arrays.copyOf(coefficients, lastNonZero + 1);
+            if (degree == 0 && coefficients[degree]== 0){
+                coeff = new int[] {0};
+            }
+
+            else {
+                coeff = Arrays.copyOf(coefficients, degree + 1);
+            }
     }
-
-    public int getDegree() {
-        return coeff.length - 1;
+    
+    public int getDegree() 
+    {
+        
+        return coeff.length - 1; 
     }
-
-    public int getCoefficient(int k) {
-        if (k >= 0 && k < coeff.length) return coeff[k];
-        return 0;
+    
+    public int getCoefficient(int k) 
+    {
+       
+        if (k >= 0 && k < coeff.length){
+            return coeff[k];
+            }
+            return 0;
     }
-
-    public long evaluate(int x) {
+    
+    public long evaluate(int x) 
+    {
+        
         long result = 0;
-        long power = 1;
-        for (int k = 0; k < coeff.length; k++) {
-            result += coeff[k] * power;
-            power *= x;
-        }
-        return result;
-    }
+        long pwr = 1;
 
-    public Polynomial add(Polynomial other) {
-        int maxDegree = Math.max(this.getDegree(), other.getDegree());
-        int[] newCoeffs = new int[maxDegree + 1];
-
-        for (int i = 0; i <= maxDegree; i++) {
-            newCoeffs[i] = this.getCoefficient(i) + other.getCoefficient(i);
+        for (int i = 0; i < coeff.length; i++){
+            result = result + (coeff[i] * (pwr));
+            pwr *= x;
         }
 
-        return new Polynomial(newCoeffs);
+        return result; 
     }
+    
+    public String toString() 
+    {
+       
+        StringBuilder p = new StringBuilder();
+            boolean firstTerm = true; 
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = coeff.length - 1; i >= 0; i--) {
-            if (coeff[i] != 0) {
-                if (sb.length() > 0) {
-                    sb.append(coeff[i] > 0 ? " + " : " - ");
-                } else if (coeff[i] < 0) {
-                    sb.append("-");
-                }
+            for (int i = this.coeff.length - 1; i >= 0; i--) {
+                if (this.coeff[i] != 0) {  
+                    if (!firstTerm) {
+                        if (this.coeff[i] > 0) p.append(" + ");
+                        else p.append(" - ");
+                    } else {  
+                        if (this.coeff[i] < 0) p.append("-");
+                    }
 
-                int absCoeff = Math.abs(coeff[i]);
-                if (i == 0 || absCoeff != 1) {
-                    sb.append(absCoeff);
-                } else {
-                    sb.append("1");
-                }
-                
-                if (i > 0) {
-                    sb.append("x");
-                    if (i > 1) sb.append("^").append(i);
+                    p.append(Math.abs(this.coeff[i])); 
+
+                    if (i > 1) p.append("x^").append(i);
+                    else if (i == 1) p.append("x");
+
+                    firstTerm = false;  
                 }
             }
+
+            return (p.length() == 0) ? "0" : p.toString();  
         }
-        return sb.length() == 0 ? "0" : sb.toString().replace("+ -", "- ");
+    
+    
+    public Polynomial add(Polynomial other) 
+    {
+        int size = Math.max(this.coeff.length, other.coeff.length);
+        int[] resultCoeffs = new int[size];
+    
+        for (int i = 0; i < size; i++) {
+            if (i < this.coeff.length) resultCoeffs[i] += this.coeff[i];
+            if (i < other.coeff.length) resultCoeffs[i] += other.coeff[i];
+        }
+    
+        return new Polynomial(resultCoeffs);
+    }
+    public Polynomial multiply(Polynomial other) 
+{
+    int newDegree = this.getDegree() + other.getDegree();
+    int[] resultCoeffs = new int[newDegree + 1];
+
+    for (int i = 0; i < this.coeff.length; i++) {
+        for (int j = 0; j < other.coeff.length; j++) {
+            resultCoeffs[i + j] += this.coeff[i] * other.coeff[j];
+        }
+    }
+
+    return new Polynomial(resultCoeffs);
+}
+    
+    @Override public boolean equals(Object other) 
+    {
+
+      if (this == other) return true;
+    if (!(other instanceof Polynomial)) return false;
+
+    Polynomial p = (Polynomial) other;
+    return Arrays.equals(this.coeff, p.coeff);
+    }
+    
+    public int compareTo(Polynomial other) 
+    {
+      
+        if (this.getDegree() > other.getDegree()) return 1;
+    if (this.getDegree() < other.getDegree()) return -1;
+
+    for (int i = this.getDegree(); i >= 0; i--) {
+        if (this.coeff[i] > other.coeff[i]) return 1;
+        if (this.coeff[i] < other.coeff[i]) return -1;
+    }
+    return 0;
     }
 }
